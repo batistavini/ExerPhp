8f46ff27e4ddd40e6ced218ca2a34c790a40c091
Vinicius Batista <viniciusdias294@gmail.com>
2024-08-08 16:18:14 -0300

    Index sendo estilizada e páginas criadas, também está salva a paleta de cores (ao menos a inicial) para iniciação do site.

---
+++ index-loja.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index - Loja</title>
+    <link rel="stylesheet" href="src/index.css">
+</head>
+<body>
+    <header>
+        <nav>
+         <a href="sobre.php">Sobre</a>
+         <a href="cadastrop.php">Cadastro de Produto</a>
+        <a href="login.php">Login</a>
+        <a href="produto.php">Produtos</a>
+        </nav>
+    </header>
+    <h1>Bem vindo a Urubu do Pix</h1>
+    
+    
+    
+    
+    
+    
+    
+    
+    <script>
+    
+
+
+
+    </script>
+</body>
+</html>
